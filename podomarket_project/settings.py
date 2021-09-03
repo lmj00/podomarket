f9d556921642dd105be7347aceb241859a600197
이민진 <e.minjin00@gmail.com>
2021-09-03 23:59:41 +0900

    Feat: MEDIA_URL, ROOT in settings.py

--- podomarket_project/settings.py
+++ podomarket_project/settings.py
@@ -128,7 +128,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-
+MEDIA_URL = '/uploads/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 AUTH_USER_MODEL = 'podomarket.User'
 
 AUTHENTICATION_BACKENDS = [
