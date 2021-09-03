41af829fefa19ba5d3ab7393bf7034b82b8ef5d9
이민진 <e.minjin00@gmail.com>
2021-09-03 23:59:17 +0900

    Feat: add Post in models.py

--- podomarket/models.py
+++ podomarket/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 from .validators import validate_no_special_characters
+from django.core.validators import MinValueValidator
 
 # Create your models here.
 class User(AbstractUser):
@@ -10,3 +11,24 @@ class User(AbstractUser):
 
     def __str__(self):
         return self.email   
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=60)
+    item_price = models.IntegerField(validators=[MinValueValidator(1)])
+
+    ITEM_CONDITION_CHOICE = [('새제품', '새제품'), ('최상', '최상'), ('상', '상'), ('중', '중'), ('하', '하')]
+    item_condition = models.CharField(choices=ITEM_CONDITION_CHOICE, max_length=5)
+
+    item_details = models.TextField(blank=True)
+    image1 = models.ImageField(upload_to='item_pics')
+    image2 = models.ImageField(upload_to='item_pics', blank=True)
+    image3 = models.ImageField(upload_to='item_pics', blank=True)
+    
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    dt_created = models.DateField(auto_now_add=True)
+    dt_updated = models.DateField(auto_now=True)
+
+
+    def __str__(self):
+        return self.title
