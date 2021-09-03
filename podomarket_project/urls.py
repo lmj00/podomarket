202e1c7740ccb0c89ea50e8e89d0952d29c071e4
이민진 <e.minjin00@gmail.com>
2021-09-04 00:00:01 +0900

    Feat: media urlpatterns in urls.py

--- podomarket_project/urls.py
+++ podomarket_project/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include, reverse
 from django.views.generic import TemplateView
+from django.conf import settings
+from django.conf.urls.static import static 
 from podomarket.views import CustomPasswordChangeView
 
 urlpatterns = [
@@ -26,3 +28,5 @@ urlpatterns = [
     path('', include('allauth.urls')),
 
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
